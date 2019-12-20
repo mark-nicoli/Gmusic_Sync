c0716ab6088f5678d7b762246cfda520ad19a692
mark-nicoli <mark21198@gmail.com>
2019-12-20 10:51:54 +0000

    update

---
+++ gmusic.py
@@ -0,0 +1,12 @@
+from gmusicapi import Mobileclient
+
+api = Mobileclient()
+
+api.perform_oauth()
+
+library = api.get_all_songs()
+sweet_track_ids = [track['id'] for track in library if track['artist'] == 'the cat empire']
+
+playlist_id = api.create_playlist('rad muzik')
+
+api.add_songs_to_playlist(playlist_id, sweet_track_ids)
